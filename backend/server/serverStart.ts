d4354437427d44bdba47997a5748d42f7339edfe
subnub <kyle.hoell@gmail.com>
2020-05-08 18:54:36 -0400

    added better heroku support

--- backend/server/serverStart.ts
+++ backend/server/serverStart.ts
@@ -22,21 +22,21 @@ const serverStart = async() => {
         } );
         
     } else if (process.env.NODE_ENV === 'production') {
+
+        const port = process.env.HTTP_PORT || process.env.PORT;
     
-        server.listen(process.env.HTTP_PORT,process.env.URL, () => {
-            console.log("Http Server (No-SSL) Running On Port:", process.env.HTTP_PORT);
+        server.listen(port, process.env.URL, () => {
+            console.log("Http Server (No-SSL) Running On Port:", port);
         })
 
     } else {
     
-        server.listen(process.env.HTTP_PORT,process.env.URL, () => {
-            console.log("Development Server Running On Port:", process.env.HTTP_PORT);
+        const port = process.env.HTTP_PORT || process.env.PORT;
+
+        server.listen(port, process.env.URL, () => {
+            console.log("Development Server Running On Port:", port);
         })
     }
 }
 
 serverStart();
-
-
-
-
