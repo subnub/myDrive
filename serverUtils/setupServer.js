2b6a4d64c1cad1e7d545d9f9ec333b26c2b948d1
subnub <kyle.hoell@gmail.com>
2020-05-21 23:16:32 -0400

    Added setup for non-docker images too

--- serverUtils/setupServer.js
+++ serverUtils/setupServer.js
@@ -102,7 +102,7 @@ const initServer = async() => {
 
         const getClientURL = await prompts({
             type: 'text',
-            message: "Enter The Client URL/IP Address (Must Be  A Valid Link)",
+            message: "Enter The Client URL/IP Address (Enter The Client URL/IP Address (Must Be A Valid Link, Include Port With IP Address If Needed)",
             name: "value"
         })
 
@@ -190,151 +190,144 @@ const initServer = async() => {
 
         await awaitWriteFile("./docker-variables.env", stringBuilder);
 
+        console.log("\nCreated Docker Env File");
+
     } else {
 
-    }
+        let stringBuilderClient = '';
+        let stringBuilderServer = '';
 
+        const getMongoURL = await prompts({
+            type: 'text',
+            message: "Enter The MongoDB URL",
+            name: "value"
+        })
 
+        const mongoURL = getMongoURL.value;
 
-    return;
+        stringBuilderServer += "MONGODB_URL=" + mongoURL + "\n";
 
-    await fs.mkdir("./config", async() => {
+        const getKeyType = await prompts({
+            type: 'toggle',
+            name: 'value',
+            message: "Use WebUI For Encryption Key (Recommended, Selecting No Will Require You To Enter An Encryption Key Now, Which Is Less Secure)",
+            initial: true,
+            active: 'yes',
+            inactive: 'no'
+        })
 
-        const getClientPortNumber = await prompts({
-            type: 'text',
-            message: "Enter Client Port Number (Default 3000):",
-            name: "value"
-        })
-        const clientPortNumber = getClientPortNumber.value || "3000";
-    
-        const getClientUrl = await prompts({
-            type: 'text',
-            message: "\nEnter Client URL (Add Port Number If Needed, Default http://localhost:3000):",
-            name: "value"
-        })
-    
-        const clientURL = getClientUrl.value || "http://localhost:3000"
-    
-        const getClientTranscode = await prompts({
-            type: 'text',
-            message: "\nEnable Transcode Video (Default False)",
-            name: "value"
-        })
-        const clientTranscode = getClientTranscode.value || "false";
-    
-        let fullInfoClient = "PORT=" + clientPortNumber + "\n" 
-        + "REMOTE_URL=" + clientURL
-    
-        console.log("client transcode", clientTranscode);
-    
-        if (clientTranscode.toLowerCase() === "true") {
-            
-            fullInfoClient = fullInfoClient + "\n" 
-            + "ENABLE_VIDEO_TRANSCODING=true"
-        }
-    
-        console.log("\nClient Into Completed, Getting Server Info...\n")
-    
-        const getSeverMongoURL = await prompts({
-            type: 'text',
-            message: "\nEnter MongoDB URL (Default mongodb://127.0.0.1:27017/personal-drive):",
-            name: "value"
-        })
-    
-        const serverMongoURL = getSeverMongoURL.value || "mongodb://127.0.0.1:27017/personal-drive"
-    
-        let randomPassword = crypto.randomBytes(16);
-        randomPassword = randomPassword.toString("hex");
-    
-    
-        const getServerPassword = await prompts({
-            type: 'text',
-            message: `\nEnter Server JSON Token Password (Default Random Password ${randomPassword}): `,
-            name: "value"
-        })
-        const serverPassword = getServerPassword.value || randomPassword;
-    
-    
-        const getServerHttpPort = await prompts({
-            type: 'text',
-            message: `\nEnter Server Http Port (Default 3000): `,
-            name: "value"
-        })
-        const serverHttpPort = getServerHttpPort.value || "3000";
-    
-    
-        const getServerHttpsPort =  await prompts({
-            type: 'text',
-            message: `\nEnter Server Https Port (Default 8080): `,
-            name: "value"
-        })
-        const serverHttpsPort = getServerHttpsPort.value || "8080"
-    
-        const getServerUrl = await prompts({
-            type: 'text',
-            message: `\nEnter Server URL (Do Not Include Port, Default localhost):`,
-            name: "value"
-        })
-        const serverURL = getServerUrl.value || "localhost"
-    
-        const getServerFullUrl = await prompts({
-            type: 'text',
-            message: `\nEnter Full Server URL (Include Port If Needed, Default http://localhost:3000):`,
-            name: "value"
-        })
-        const serverFullURL = getServerFullUrl.value || "http://localhost:3000";
-    
-        const getServerRoot = await prompts({
-            type: 'text',
-            message: `\nEnter Path For Storage Size Check (Default /):`,
-            name: "value"
-        })
-        const serverRoot = getServerRoot.value || "/";
-    
-        const fullServerInfo = "MONGODB_URL=" + serverMongoURL + "\n" 
-        + "PASSWORD=" + serverPassword + "\n" 
-        + "HTTP_PORT=" + serverHttpPort + "\n"
-        + "HTTPS_PORT=" + serverHttpsPort + "\n"
-        + "URL=" + serverURL + "\n" 
-        + "FULL_URL=" + serverFullURL + "\n"
-        + "ROOT=" + serverRoot + "\n";
-    
-        console.log("\nServer Info Finished, Displaying Results...\n");
-    
-        console.log("Client Info:\n", fullInfoClient)
-    
-        console.log("\nServer Info:\n", fullServerInfo)
-    
-        const getConfimation = await prompts({
-            type: 'text',
-            message: `Would You Like To Save These Files (Yes/No): `,
-            name: "value"
-        })
-    
-        if (!getConfimation.value || getConfimation.value.toLowerCase() !== "yes") {
-            console.log("Exiting...")
-            process.exit()
-            return;
-        }
-    
-        await fs.writeFile("./.env.production", fullInfoClient, async(err) => {
-    
-            if (err) {
-                console.log("Error", err);
-                process.exit();
-            }
-    
-            await fs.writeFile("./config/prod.env", fullServerInfo, (err2) => {
-    
-                if (err2) {
-                    console.log("Error", err2);
-                    process.exit();
-                }
+        let keyType = getKeyType.value;
+
+        if (!keyType) {
+
+            const getKey = await prompts({
+                type: 'password',
+                message: "Enter The Encryption Key",
+                name: "value"
             })
-            
+
+            const key = getKey.value;
+
+            stringBuilderServer += "KEY=" + key + "\n";
+        }
+
+        const getClientURL = await prompts({
+            type: 'text',
+            message: "Enter The Client URL/IP Address (Must Be A Valid Link, Include Port With IP Address If Needed)",
+            name: "value"
         })
 
-    });
+        const clientURL = getClientURL.value;
+
+        stringBuilderClient += "REMOTE_URL=" + clientURL + "\n";
+
+        const getChunkDB = await prompts({
+            type: 'select',
+            name: 'value',
+            message: 'Pick A Database To Store File Chunks',
+            choices: [
+              { title: 'Amazon S3', value: 's3'},
+              { title: 'FileSystem', value: 'fs'},
+              { title: 'MongoDB', value: 'mongo' },
+            ],
+            initial: 1
+        })
+
+        const chunkDB = getChunkDB.value;
+
+        stringBuilderServer += "DB_TYPE=" + chunkDB + "\n";
+
+        if (chunkDB === "s3") {
+
+            const gets3ID = await prompts({
+                type: 'text',
+                message: "Enter The S3 ID",
+                name: "value"
+            })
+
+            const s3ID = gets3ID.value;
+
+            stringBuilderServer += "S3_ID=" + s3ID + "\n";
+
+            const gets3Key = await prompts({
+                type: 'password',
+                message: "Enter The S3 Key",
+                name: "value"
+            })
+
+            const s3Key = gets3Key.value;
+            
+            stringBuilderServer += "S3_KEY=" + s3Key + "\n";
+
+            const gets3Bucket = await prompts({
+                type: 'text',
+                message: "Enter The S3 Bucket",
+                name: "value"
+            })
+
+            const s3Bucket = gets3Bucket.value;
+
+            stringBuilderServer += "S3_BUCKET=" + s3Bucket + "\n";
+            stringBuilderClient += "DISABLE_STORAGE=true\n";
+            
+        } else if (chunkDB === "fs") {
+
+            const getFSPath = await prompts({
+                type: 'text',
+                message: "Enter The FileSystem Path",
+                name: "value"
+            })
+
+            const fsPath = getFSPath.value;
+
+            stringBuilderServer += "FS_DIRECTORY=" + fsPath + "\n";
+            stringBuilderServer += "ROOT=" + fsPath + "\n"; 
+
+        } else {
+
+            stringBuilderClient += "DISABLE_STORAGE=true\n";
+        }
+
+        const getJWTSecret = await prompts({
+            type: 'password',
+            message: "Enter JWT Secret",
+            name: "value",
+        })
+
+        const JWTsecret = getJWTSecret.value;
+
+        stringBuilderServer += "PASSWORD=" + JWTsecret + "\n";
+
+        stringBuilderServer += "NODE_ENV=production\n";
+        stringBuilderClient += "PORT=3000\n";
+        stringBuilderServer += "HTTP_PORT=3000\n"; 
+        stringBuilderServer += "HTTPS_PORT=8080\n"
+
+        await awaitWriteFile("./.env.production", stringBuilderClient);
+        await awaitWriteFile("./config/prod.env", stringBuilderServer);
+
+        console.log("\nServer And Client Env Files Created");
+    }
 }
 
 initServer();
