346270139439952358851caee095d5589390fcad
subnub <kyle.hoell@gmail.com>
2024-06-08 21:13:22 -0400

    updating depends

---
+++ vite.config.js
@@ -0,0 +1,11 @@
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+
+export default defineConfig({
+  plugins: [react()],
+  server: {
+    proxy: {
+      "/api": "http://localhost:3000", // Proxy API requests to Express server
+    },
+  },
+});
