0d80941e22706320c4f757bf5156c76ba109181c
subnub <kyle.hoell@gmail.com>
2024-10-19 01:12:57 -0400

    cleaned up store

--- src/store/configureStore.ts
+++ src/store/configureStore.ts
@@ -1,55 +1,17 @@
 import { configureStore } from "@reduxjs/toolkit";
-import authReducer from "../reducers/auth";
-import mainReducer from "../reducers/main";
-import fileReducer from "../reducers/files";
-import folderReducer from "../reducers/folders";
 import filterReducer from "../reducers/filter";
-import selectedItemReducer from "../reducers/selectedItem";
-import uploadsReducer from "../reducers/uploads";
-import storageReducer from "../reducers/storage";
-import quickFilesReducer from "../reducers/quickFiles";
-import popupFilesReducer from "../reducers/popupFile";
-import settingsReducer from "../reducers/settings";
-import parentReducer from "../reducers/parent";
-import addOptionsReducer from "../reducers/addOptions";
-import photoViewerReducer from "../reducers/photoViewer";
-import routesReducer from "../reducers/routes";
-import moverReducer from "../reducers/mover";
-import folderTreeReducer from "../reducers/folderTree";
-import uploadStorageSwitcherReducer from "../reducers/uploadStorageSwitcher";
-import mobileContextMenuReducer from "../reducers/mobileContextMenu";
 import selectedReducer from "../reducers/selected";
 import leftSectionReducer from "../reducers/leftSection";
 import userReducer from "../reducers/user";
 import uploaderReducer from "../reducers/uploader";
 
-//const composeEnchancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
-
 const store = configureStore({
   reducer: {
-    auth: authReducer,
-    main: mainReducer,
-    // files: fileReducer,
-    // folders: folderReducer,
     filter: filterReducer,
     selected: selectedReducer,
     leftSection: leftSectionReducer,
-    // selectedItem: selectedItemReducer,
-    uploads: uploadsReducer,
     user: userReducer,
     uploader: uploaderReducer,
-    // storage: storageReducer,
-    // quickFiles: quickFilesReducer,
-    // popupFile: popupFilesReducer,
-    // settings: settingsReducer,
-    // parent: parentReducer,
-    // addOptions: addOptionsReducer,
-    // photoViewer: photoViewerReducer,
-    // routes: routesReducer,
-    // mover: moverReducer,
-    // folderTree: folderTreeReducer,
-    // storageSwitcher: uploadStorageSwitcherReducer,
-    // mobileContextMenu: mobileContextMenuReducer,
   },
 });
 
